658042a2fb46837d1a20980e7535cd5259d31cc4
MSI <ksidhukhushpreet@gmail.com>
2023-11-08 16:44:39 -0800

    fixed some syntac issues in Audit_log/app/py

--- Audit_log/app.py
+++ Audit_log/app.py
@@ -84,8 +84,8 @@ def get_clock_out_reading(index):
         return { "message": "Not Found" }, 404
 
 app = connexion.FlaskApp(__name__, specification_dir='')
+CORS(app.app)
 app.app.config['CORS_HEADERS'] = 'Content-Type'
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 if __name__ == "__main__":
     app.run(port=8110)
