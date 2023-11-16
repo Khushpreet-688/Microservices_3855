1311b0d30129f8a41d4f5ea063947550db6bf279
MSI <ksidhukhushpreet@gmail.com>
2023-11-16 09:59:30 -0800

    testing

--- Receiver/app.py
+++ Receiver/app.py
@@ -22,7 +22,23 @@ with open('log_conf.yml', 'r') as f:
 
 logger = logging.getLogger('basicLogger')
 
+max_retries = app_config["connection"]["max_retries"]
+sleep_time = app_config["connection"]["sleep_time"]
 
+retry_count = 0
+
+while retry_count < max_retries: 
+    try:
+        logger.info(f'Trying to reconnect to Kafka. Retry count: {retry_count}')
+        client = KafkaClient(hosts=f'{app_config["events"]["hostname"]}:{app_config["events"]["port"]}')
+        TOPIC = client.topics[str.encode(app_config["events"]["topic"])]
+        producer = TOPIC.get_sync_producer()
+        logger.info("Connected!")
+        break
+    except:
+        logger.error("Connection failed. Retrying ...")
+        time.sleep(sleep_time)
+        retry_count += 1
 
 
 
@@ -42,7 +58,7 @@ def clock_in(body):
             "datetime" : datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
             "payload": body }
     msg_str = json.dumps(msg)
-    PRODUCER.produce(msg_str.encode('utf-8'))
+    producer.produce(msg_str.encode('utf-8'))
 
     # if response.status_code == 201:
     #     print(response)
@@ -68,7 +84,7 @@ def clock_out(body):
             "datetime" : datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
             "payload": body }
     msg_str = json.dumps(msg)
-    PRODUCER.produce(msg_str.encode('utf-8'))
+    producer.produce(msg_str.encode('utf-8'))
 
     logger.info(f"Returned event {event} response {trace_id} with status 201")
     return NoContent, 201
@@ -97,24 +113,6 @@ def log_events(body, data_str):
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 if __name__ == "__main__":
-    max_retries = app_config["connection"]["max_retries"]
-    sleep_time = app_config["connection"]["sleep_time"]
-
-    retry_count = 0
-
-    while retry_count < max_retries: 
-        try:
-            logger.info(f'Trying to reconnect to Kafka. Retry count: {retry_count}')
-            client = KafkaClient(hosts=f'{app_config["events"]["hostname"]}:{app_config["events"]["port"]}')
-            TOPIC = client.topics[str.encode(app_config["events"]["topic"])]
-            PRODUCER = TOPIC.get_sync_producer()
-            logger.info("Connected!")
-            break
-        except:
-            logger.error("Connection failed. Retrying ...")
-            time.sleep(sleep_time)
-            retry_count += 1
-            
     app.run(port=8080)
     
 
