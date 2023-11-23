5d6bac2d3303bafd235d3b3f2adaa7712e8e31d1
MSI <ksidhukhushpreet@gmail.com>
2023-11-23 09:50:42 -0800

    added configuration for reverse proxy

--- Processing/app.py
+++ Processing/app.py
@@ -110,9 +110,10 @@ def init_scheduler():
     sched.start()
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+    CORS(app.app)
+    app.app.config['CORS_HEADERS'] = 'Content-Type'
+app.add_api("openapi.yaml", base_path="/processing", strict_validation=True, validate_responses=True)
 if __name__ == "__main__":
     init_scheduler()
     app.run(port=8100, use_reloader=False)
--- Receiver/app.py
+++ Receiver/app.py
@@ -123,7 +123,7 @@ def log_events(body, data_str):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", base_path="/receiver", strict_validation=True, validate_responses=True)
 if __name__ == "__main__":
     app.run(port=8080)
     
